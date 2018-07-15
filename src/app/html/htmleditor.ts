243b63dc54bac4c9650d2360c5a1a5a68e4bd563
YoshiDesign <30674964+YoshiDesign@users.noreply.github.com>
2018-07-15 18:01:56 -0400

    optimizing html injection

--- src/app/html/htmleditor.ts
+++ src/app/html/htmleditor.ts
@@ -1,7 +1,6 @@
 export default class HTMLeditors {
     /**
-    *   Future Extensibility - The JSON keys in editorNodes 
-    *   are currently free parking; arbitrary and non-referenced.
+    *   Relationships between our selected form and its available options / subsets
     */
     constructor(){}
 
@@ -9,25 +8,32 @@ export default class HTMLeditors {
         switch (idBy) {
             // These ID's should become classes upon extension
             case "nameField":
-                return String(`{
-                    "nameField" : 
-                        "<input id='nglabeling' type='text' placeholder='Edit Label'>"
-                }`);
+                let x = document.createElement("INPUT");
+                x.setAttribute("id", "nglabeling");
+                x.setAttribute("type", "text");
+                x.setAttribute("placeholder", "Edit Label");
+                console.log("XXXXX" + x.id);
+                return x;
+                // return String(`{
+                //     "nameField" : 
+                //         "<input id='nglabeling' type='text' placeholder='Edit Label' >"
+                        
+                // }`);
                 // break;
-            case "passwdField":
-                return null;
-                // break;
-            case "emailField":
-                return String(`{
-                    "emailField" : 
-                        "<input id='nglabeling' type='text' value='' placeholder='Edit Label'>"
-                }`);
-                // break;
-            case "checkboxField":
-                return String(`{
-                    "checkboxField" : 
-                        "<input id='nglabeling' type='text' value='' id='checkboxEdit' placeholder='Edit Label'>"
-                }`);
+            // case "passwdField":
+            //     return null;
+            //     // break;
+            // case "emailField":
+            //     return String(`{
+            //         "emailField" : 
+            //             "<input id='nglabeling' type='text' value='' placeholder='Edit Label'>"
+            //     }`);
+            //     // break;
+            // case "checkboxField":
+            //     return String(`{
+            //         "checkboxField" : 
+            //             "<input id='nglabeling' type='text' value='' id='checkboxEdit' placeholder='Edit Label'>"
+            //     }`);
 
             default:
                 return null;
@@ -35,60 +41,74 @@ export default class HTMLeditors {
 
     }
 
-    // public selectOptions : NodeList = [
-    //         "<option value='None'>Select State</option>",
-    //         "<option value='INTL'>International Customer</option>",
-    //         "<option value='AL'>Alabama</option>",
-    //         "<option value='AK'>Alaska</option>",
-    //         "<option value='AZ'>Arizona</option>",
-    //         "<option value='AR'>Arkansas</option>",
-    //         "<option value='CA'>California</option>",
-    //         "<option value='CO'>Colorado</option>",
-    //         "<option value='CT'>Connecticut</option>",
-    //         "<option value='DE'>Delaware</option>",
-    //         "<option value='DC'>District Of Columbia</option>",
-    //         "<option value='FL'>Florida</option>",
-    //         "<option value='GA'>Georgia</option>",
-    //         "<option value='HI'>Hawaii</option>",
-    //         "<option value='ID'>Idaho</option>",
-    //         "<option value='IL'>Illinois</option>",
-    //         "<option value='IN'>Indiana</option>",
-    //         "<option value='IA'>Iowa</option>",
-    //         "<option value='KS'>Kansas</option>",
-    //         "<option value='KY'>Kentucky</option>",
-    //         "<option value='LA'>Louisiana</option>",
-    //         "<option value='ME'>Maine</option>",
-    //         "<option value='MD'>Maryland</option>",
-    //         "<option value='MA'>Massachusetts</option>",
-    //         "<option value='MI'>Michigan</option>",
-    //         "<option value='MN'>Minnesota</option>",
-    //         "<option value='MS'>Mississippi</option>",
-    //         "<option value='MO'>Missouri</option>",
-    //         "<option value='MT'>Montana</option>",
-    //         "<option value='NE'>Nebraska</option>",
-    //         "<option value='NV'>Nevada</option>",
-    //         "<option value='NH'>New Hampshire</option>",
-    //         "<option value='NJ'>New Jersey</option>",
-    //         "<option value='NM'>New Mexico</option>",
-    //         "<option value='NY'>New York</option>",
-    //         "<option value='NC'>North Carolina</option>",
-    //         "<option value='ND'>North Dakota</option>",
-    //         "<option value='OH'>Ohio</option>",
-    //         "<option value='OK'>Oklahoma</option>",
-    //         "<option value='OR'>Oregon</option>",
-    //         "<option value='PA'>Pennsylvania</option>",
-    //         "<option value='RI'>Rhode Island</option>",
-    //         "<option value='SC'>South Carolina</option>",
-    //         "<option value='SD'>South Dakota</option>",
-    //         "<option value='TN'>Tennessee</option>",
-    //         "<option value='TX'>Texas</option>",
-    //         "<option value='UT'>Utah</option>",
-    //         "<option value='VT'>Vermont</option>",
-    //         "<option value='VA'>Virginia</option>",
-    //         "<option value='WA'>Washington</option>",
-    //         "<option value='WV'>West Virginia</option>",
-    //         "<option value='WI'>Wisconsin</option>",
-    //         "<option value='WY'>Wyoming</option>",
-    // ]
+    public selectOptions = {
+        // Locale Select box by state
+        "None":"Select State",
+        'INTL':"International Customer",
+        'AL':"Alabama",
+        'AK':"Alaska",
+        'AZ':"Arizona",
+        'AR':"Arkansas",
+        'CA':"California",
+        'CO':"Colorado",
+        'CT':"Connecticut",
+        'DE':"Delaware",
+        'DC':"District Of Columbia",
+        'FL':"Florida",
+        'GA':"Georgia",
+        'HI':"Hawaii",
+        'ID':"Idaho",
+        'IL':"Illinois",
+        'IN':"Indiana",
+        'IA':"Iowa",
+        'KS':"Kansas",
+        'KY':"Kentucky",
+        'LA':"Louisiana",
+        'ME':"Maine",
+        'MD':"Maryland",
+        'MA':"Massachusetts",
+        'MI':"Michigan",
+        'MN':"Minnesota",
+        'MS':"Mississippi",
+        'MO':"Missouri",
+        'MT':"Montana",
+        'NE':"Nebraska",
+        'NV':"Nevada",
+        'NH':"New Hampshire",
+        'NJ':"New Jersey",
+        'NM':"New Mexico",
+        'NY':"New York",
+        'NC':"North Carolina",
+        'ND':"North Dakota",
+        'OH':"Ohio",
+        'OK':"Oklahoma",
+        'OR':"Oregon",
+        'PA':"Pennsylvania",
+        'RI':"Rhode Island",
+        'SC':"South Carolina",
+        'SD':"South Dakota",
+        'TN':"Tennessee",
+        'TX':"Texas",
+        'UT':"Utah",
+        'VT':"Vermont",
+        'VA':"Virginia",
+        'WA':"Washington",
+        'WV':"West Virginia",
+        'WI':"Wisconsin",
+        'WY':"Wyoming"
+    }
 
+    public styles = {
+        "major" : `
+            width:90%;
+            border-radius: 0;
+            border: none;
+            border-bottom: 1px solid #fff;
+            height: 40px;
+            background-color: rgba(255, 255, 255, .1);
+            color: #fff;
+            padding: 0 0 0 5px;
+            font-size: 25px;`,
+        
+    }
 }
--- src/app/renderable/renderable-item/renderable-item.component.ts
+++ src/app/renderable/renderable-item/renderable-item.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Renderer } from '../../model/renderer';
 import Editor from '../../html/htmleditor';
 
 @Component({
@@ -9,45 +8,56 @@ import Editor from '../../html/htmleditor';
 })
 export class RenderableItemComponent implements OnInit {
   
+  public form   : Element;
+  public editor : Editor;
+  public illegalInputEvent  : RegExp;
+  public illegalInputScript : RegExp;
+  
   constructor() {}
 
   ngOnInit() {
-    
+    this.editor = new Editor;
+    this.illegalInputEvent = /"^on+$"/;
+    this.illegalInputScript = /"[<script>]"/;
+    this.form = document.getElementById('lead-gen-form-input');
   }
 
-  toggleAdd(event, elem : string, identifiedBy : string){
-    /* 
-      Adds visible element to middle column 
-      +localize 'attr' with scope visibility 
+  toggleAdd(event, elem : string, identifiedBy : string, otherOpt? : number){
+    /** 
+    *   Adds visible element to middle column.
+    *   otherOpt is a universal flag for transitive state.
+    *   +localize 'attr' with scope visibility.
     */
-
+    let newInput : Element; // Encapsulated per the page spec.
     if (elem == null || undefined) {
       // Do nothing
       return;
     }
 
-    let newInput : Element; // Encapsulated per the spec.
-    var form = document.getElementById('lead-gen-form-input');
-    
     // Create Editor View
-    this.openEditor(identifiedBy, form);
+    this.openEditor(identifiedBy);
 
     // Returns a formatted input view
-    newInput = this.makeField(identifiedBy, elem);
+    if (!otherOpt)
+      newInput = this.makeField(identifiedBy, elem);
+    else // otherOpt is a universal flag. 
+      newInput = this.makeField(identifiedBy, elem, 1);
 
     // Send new input to the next available field
-    form.appendChild(newInput);
+    this.form.appendChild(newInput);
 
     event.stopPropagation();
   }
   
 ///////////////////////Remember to Delete Editor upon completion///////////////////////////////////
-  makeField = (idBy : string, elem : string) : Element => {
+  makeField = (idBy : string, elem : string, otherOpt? : number) : Element => {
 
     let newInput : Element;
     let auxNodes : Element; // i.e. <option> units
     let encap    : Element = document.createElement("P");
     let label    : Element = document.createElement("LABEL");
+
+    label.classList.add("ng-anchor");
 
     if (elem == "select")
       var input : Element = document.createElement("SELECT");
@@ -62,6 +72,10 @@ export class RenderableItemComponent implements OnInit {
     switch (idBy) {
 
       case "nameField":
+        if(otherOpt)
+          label.textContent = "Last Name";
+        else
+          label.textContent = "First Name";
         console.log("ENCAP" + encap);
         input.setAttribute('type', elem);
         encap.appendChild(label);
@@ -69,18 +83,21 @@ export class RenderableItemComponent implements OnInit {
         return encap;
 
       case "emailField":
+        label.textContent = "Email";
         input.setAttribute('type', elem);
         encap.appendChild(label);
         encap.appendChild(input);
         return encap;
 
       case "telField":
+        label.textContent = "Phone Number (format: XXX-XXX-XXXX)";
         input.setAttribute('type', elem);
         encap.appendChild(label);
         encap.appendChild(input);
         return encap;
 
       case "passwdField":
+        label.textContent = "Password";
         input.setAttribute('type', elem);
         encap.appendChild(label);
         encap.appendChild(input);
@@ -129,9 +146,21 @@ export class RenderableItemComponent implements OnInit {
         return encap;
         
       case "selectField":
+        let getLocale = new Editor;
+        let places = getLocale.selectOptions;
+        for (let i in places){
+          // Recycling variable to append locale to select box. places[loc] = Literal location name
+          var newOption = document.createElement("OPTION");
+          newOption.setAttribute("value", <string> i);
+          newOption.innerText = <string> places[i];
+          input.appendChild(newOption);
+        }
+        label.textContent = "State";
         input.setAttribute('type', elem);
         encap.appendChild(label);
         encap.appendChild(input);
+        console.log(`LOCALE ITEMS : ${places}`);
+        
         return encap;
 
       case "textareaField":
@@ -141,57 +170,68 @@ export class RenderableItemComponent implements OnInit {
         return encap;
 
       default:
-        break;
+        console.log("Improbability Alert : failing quietly");
+        return null;
     }
-    return encap;
   }
 
-  openEditor = (idBy : string, form : Element):void => {
-    var editor       = new (Editor);
-    var editorWindow = document.getElementById("in-editor");
-    let newEditorEls = JSON.parse(String(editor.editorNodes(idBy))) || null; // Contains an object of templates to render in the editor pane
+  openEditor = (idBy : string) : void => {
     
+    var editorWindow = document.getElementById("in-editor");
+    // We could also build html elements as pure strings (bad practice but an alternative if ever needed)
+    // let newEditorEls = JSON.parse(<string>this.editor.editorNodes(idBy)) || null;
+
+    let newEditorEls = this.editor.editorNodes(idBy);
+
     // null == undefined
     if (newEditorEls == null) {
-      editorWindow.innerHTML = "";
+      editorWindow.innerHTML = "<span>No editing options</span>";
       return;
     }
-
+    
     // Apply Template to right
-    editorWindow.innerHTML = newEditorEls[idBy];
+    editorWindow.appendChild(newEditorEls);
 
     if (editorWindow.innerHTML)
     {
-      let inField = document.getElementById("nglabeling");
-      inField.addEventListener('input', (e)=>{this.changeLabel(e, idBy, form)} );
-    } else return;
+      /**
+       * nglabeling is the EDITOR'S CURRENT INPUT FIELD.
+       * Change to class list for future extensibility.
+       *           This is a refactor point.
+       */
+      let inField = document.getElementById("nglabeling"); 
+      inField.addEventListener('input', (e)=>{this.changeLabel(e, idBy)} );
+    } 
+    else return;
+
     console.log(newEditorEls);
 
   }
 
-  changeLabel(e , idBy : string, form : Element) : void {
+  changeLabel(e , idBy : string) : void {
 
     /**
-     *  Part of the Editor's functionality and allows us 
-     *  to edit the label of the most recently added element
-     * 'e' refers to the previous stack frame's (function's) event listener, 'input'
+     *  Part of the Editor's functionality and (currently) allows us 
+     *  to edit the label of the most recently added element.
+     *  The event variable is of the next lower ordered functions stack frame
      */
 
-    // Most recent elem added
-    let lastFormLabel = form
-        .lastElementChild
-        .previousElementSibling.previousElementSibling;
+    let FormLabels : NodeList;
+    // Most recently added elem's <label>
+    let lastFormLabel : Node;
 
+    // ng-anchor(s) exist on each of the rendering form's labels
+    FormLabels = document.querySelectorAll(".ng-anchor");
+    console.log(`FormLabels length : ${FormLabels.length}`);
+    lastFormLabel = FormLabels[FormLabels.length - 1];
     console.log(`LAST FORM LABEL ${lastFormLabel}`);
-    
-    lastFormLabel.innerHTML = e.target.value;
+
+    lastFormLabel.textContent = e.target.value;
     
     console.log("changeDetected");
   }
 
-
-
-  toCapcha(form : Element) {
+  addCapcha() : void {
     /*
        Recaptcha factory function
        Gets called post form completion
@@ -203,10 +243,13 @@ export class RenderableItemComponent implements OnInit {
     reCapchaElement.setAttribute('data-sitekey', '6Lc3HWQUAAAAAAmrhK6RI77MSoHAmRH__OxEDDeB');
 
   // append our completed captcha to the form
-    form.appendChild(reCapchaElement);
+    this.form.appendChild(reCapchaElement);
 
   }
-
+  finalize() : Number {
+    this.addCapcha()
+    return 0;
+  }
   setStyle(attribute) {
 
     var style = attribute;
